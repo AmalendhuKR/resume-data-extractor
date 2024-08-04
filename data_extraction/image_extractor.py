a48505e214dc108e281b1d1b8b4f3f89e8675fe0
Amalendhu K R <amalendhu@lektik.com>
2024-08-04 21:13:45 +0530

    pre-process and extract data from images using tesseract-ocr

--- data_extraction/image_extractor.py
+++ data_extraction/image_extractor.py
@@ -1,12 +1,42 @@
+import cv2
+import numpy as np
 from PIL import Image
-
 import pytesseract
+
+def preprocess_image(img):
+     # Convert PIL image to OpenCV format (if needed)
+    output_img = np.array(img)
+    output_img = cv2.cvtColor(output_img, cv2.COLOR_RGB2BGR)
+    
+    # Grayscale conversion
+    output_img = cv2.cvtColor(output_img, cv2.COLOR_BGR2GRAY)
+    
+    # Noise reduction with Gaussian Blur
+    output_img = cv2.GaussianBlur(output_img, (5, 5), 0)
+    
+    # Thresholding to create binary image
+    _, output_img = cv2.threshold(output_img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    
+    # Further noise reduction with dilation and erosion
+    kernel = np.ones((3, 3), np.uint8)
+    output_img = cv2.morphologyEx(output_img, cv2.MORPH_OPEN, kernel, iterations=2)
+    
+    # Ensure text is black and background is white
+    output_img = cv2.bitwise_not(output_img)
+    
+    # Convert back to PIL image format
+    output_img = Image.fromarray(output_img)
+    
+    return output_img
+
 
 def extract_text_from_image(file_path):
     text = ""
     try:
         image = Image.open(file_path)
-        text = pytesseract.image_to_string(image)
+        preprocessed_image = preprocess_image(image)
+        text = pytesseract.image_to_string(preprocessed_image, lang='eng', config='--psm 11')
+        print("Extracting text from image")
     except Exception as e:
         print(f"Error reading image {file_path}: {e}")
     return text
--- main.py
+++ main.py
@@ -5,7 +5,24 @@ from config import INPUT_PATH_PDF, INPUT_PATH_IMAGE, OUTPUT_PATH
 from data_extraction.pdf_extractor import extract_text_from_pdf
 from data_extraction.image_extractor import extract_text_from_image
 
-def process_resumes(folder_path):
+def process_image_resumes(folder_path):
+    output_folder=OUTPUT_PATH+"/images"
+    os.makedirs(output_folder, exist_ok=True)
+    for file_name in os.listdir(folder_path):
+        file_path = os.path.join(folder_path, file_name)
+        
+        if file_path.endswith('.jpg') or file_path.endswith('.png'):
+            file_content = extract_text_from_image(file_path)
+        
+        output_file_name = os.path.splitext(file_name)[0] + '.txt'
+        output_file_path = os.path.join(output_folder, output_file_name)
+
+        with open(output_file_path, 'w', encoding='utf-8') as output_file:
+            output_file.write(file_content)
+
+    print("Successfully generated the result")
+
+def process_pdf_resumes(folder_path):
     output_folder=OUTPUT_PATH+"/pdfs"
     os.makedirs(output_folder, exist_ok=True)
     for file_name in os.listdir(folder_path):
@@ -20,11 +37,12 @@ def process_resumes(folder_path):
         with open(output_file_path, 'w', encoding='utf-8') as output_file:
             output_file.write(file_content)
 
-        print("Successfully generated the result")
+    print("Successfully generated the result")
+
 
 def main():
-    process_resumes(INPUT_PATH_PDF)
-    # print(extract_text_from_image(os.path.join(INPUT_PATH_IMAGE, "image_01.jpg")))
+    process_image_resumes(INPUT_PATH_IMAGE)
+    # print(extract_text_from_image(os.path.join(INPUT_PATH_IMAGE, "resume_02.jpg")))
 
 if __name__ == "__main__":
     main()
